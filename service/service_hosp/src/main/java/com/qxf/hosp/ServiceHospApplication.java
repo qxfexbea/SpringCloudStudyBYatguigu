43535b45d94d5c34b79fcbfd4f0276352831d091
qxfexbea <qinxianfu123@163.com>
2022-05-25 23:15:20 +0800

    P17 添加全局swagger2

--- service/service_hosp/src/main/java/com/qxf/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/qxf/hosp/ServiceHospApplication.java
@@ -2,8 +2,10 @@ package com.qxf.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.qxf")
 public class ServiceHospApplication {
 
     public static void main(String[] args) {
--- service/service_hosp/src/main/java/com/qxf/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/qxf/hosp/controller/HospitalSetController.java
@@ -31,4 +31,81 @@ public class HospitalSetController {
             List<HospitalSet> list = hospitalSetService.list();
             return list;
         }
+//    //2 逻辑删除医院设置
+//    @ApiOperation(value = "逻辑删除医院设置")
+//    @DeleteMapping("{id}")
+//    public Result removeHospSet(@PathVariable Long id) {
+//        boolean flag = hospitalSetService.removeById(id);
+//        if(flag) {
+//            return Result.ok();
+//        } else {
+//            return Result.fail();
+//        }
+//    }
+//    //3 条件查询带分页
+//    @PostMapping("findPageHospSet/{current}/{limit}")
+//    public Result findPageHospSet(@PathVariable long current,
+//                                  @PathVariable long limit,
+//                                  @RequestBody
+//                                          (required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+//        //创建page对象，传递当前页，每页记录数
+//        Page<HospitalSet> page = new Page<>(current,limit);
+//        //构建条件
+//        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+//        String hosname = hospitalSetQueryVo.getHosname();//医院名称
+//        String hoscode = hospitalSetQueryVo.getHoscode();//医院编号
+//        if(!StringUtils.isEmpty(hosname)) {
+//            wrapper.like("hosname",hospitalSetQueryVo.getHosname());
+//        }
+//        if(!StringUtils.isEmpty(hoscode)) {
+//            wrapper.eq("hoscode",hospitalSetQueryVo.getHoscode());
+//        }
+//        //调用方法实现分页查询
+//        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, wrapper);
+//        //返回结果
+//        return Result.ok(pageHospitalSet);
+//    }
+//
+//    //4 添加医院设置
+//    @PostMapping("saveHospitalSet")
+//    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet) {
+//        //设置状态 1 使用 0 不能使用
+//        hospitalSet.setStatus(1);
+//        //签名秘钥
+//        Random random = new Random();
+//        hospitalSet.setSignKey(MD5.encrypt(System.currentTimeMillis()+""+random.nextInt(1000)));
+//        //调用service
+//        boolean save = hospitalSetService.save(hospitalSet);
+//        if(save) {
+//            return Result.ok();
+//        } else {
+//            return Result.fail();
+//        }
+//    }
+//
+//    //5 根据id获取医院设置
+//    @GetMapping("getHospSet/{id}")
+//    public Result getHospSet(@PathVariable Long id) {
+//        HospitalSet hospitalSet = hospitalSetService.getById(id);
+//        return Result.ok(hospitalSet);
+//    }
+//
+//    //6 修改医院设置
+//    @PostMapping("updateHospitalSet")
+//    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet) {
+//        boolean flag = hospitalSetService.updateById(hospitalSet);
+//        if(flag) {
+//            return Result.ok();
+//        } else {
+//            return Result.fail();
+//        }
+//    }
+//
+//    //7 批量删除医院设置
+//    @DeleteMapping("batchRemove")
+//    public Result batchRemoveHospitalSet(@RequestBody List<Long> idList) {
+//        hospitalSetService.removeByIds(idList);
+//        return Result.ok();
+//    }
+
 }
