c945ab0f6b36fb75efc919061591c979e828cb2b
qxfexbea <qinxianfu123@163.com>
2022-05-25 14:58:05 +0800

    P14

---
+++ service/service_hosp/src/test/java/com/qxf/hosp/ServiceHospApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qxf.hosp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHospApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
